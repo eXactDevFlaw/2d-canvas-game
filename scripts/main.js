849f63fedeb42355353cef7667109bf80d33808d
eXactDevFlaw <lutzboelling@gmx.de>
2025-08-29 23:19:45 +0200

    feat: added new method for using assets

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "./assets/images/character/test/shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+
+let gameFrame = 0;
+const staggerFrames = 5;
+
+const spriteAnimations = [];
+const animationStates = [
+    {name: 'idle', frames: 7},
+    {name: 'jump', frames: 7},
+    {name: 'fall', frames: 7},
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        location: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.location.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+
+console.log(spriteAnimations);
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position = Math.floor(gameFrame / staggerFrames) % 6;
+  frameX = spriteWidth * position;
+
+  // ctx.fillRect(20, 50, 100, 100);
+  // ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+
+
+
+  ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+animate();
